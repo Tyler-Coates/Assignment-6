7044e51d6c1997fd5b6767046d83751dc52fa136
TylerCoates <Tyler.coates@ocsbstudent.ca>
2021-01-11 16:06:01 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("Submit").addEventListener("click", determainer);
+function determainer() {
+  document.getElementById("Pizza size").value;
+  document.getElementById("Toppings").value;
